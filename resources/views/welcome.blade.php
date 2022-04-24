61dd36e744b3e6c55851a714514a13561dba0b4f
DESKTOP-Q4JBV6J\syu72 <syu7222@gmail.com>
2022-04-25 00:01:08 +0900

    コミット（テスト）

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -119,7 +119,7 @@
                             </svg>
 
                             <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
-                                Sponsor
+                                テスト
                             </a>
                         </div>
                     </div>
