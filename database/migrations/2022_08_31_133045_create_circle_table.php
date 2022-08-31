1ed7db22408934809756bd3a8aae0bb09a9b8ec9
DESKTOP-Q4JBV6J\syu72 <syu7222@gmail.com>
2022-08-31 22:43:41 +0900

    table作成（post, circle, category）

---
+++ database/migrations/2022_08_31_133045_create_circle_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('circle', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('category_id');
+            $table->string('name', 30);
+            $table->integer('member')->length(3);
+            $table->string('comment', 255)->comment('紹介文');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('circle');
+    }
+};
